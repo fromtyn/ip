4ec8e9f8038449efaa37f6dba89961e6373e07b6
fromtyn <tianyini2023@gmail.com>
2026-01-30 23:59:54 +1100

    Complete Level 3 Mark as Done

--- src/main/java/Elden.java
+++ src/main/java/Elden.java
@@ -8,7 +8,7 @@ public class Elden {
         System.out.println("____________________________________________________________");
 
         Scanner in = new Scanner(System.in);
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int taskCount = 0;
 
         while(true) {
@@ -20,20 +20,52 @@ public class Elden {
 
             if (input.equals("list")) {
                 System.out.println("____________________________________________________________");
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < taskCount; i++) {
-                    System.out.println(i + 1 + ". " + tasks[i]);
+                    System.out.print(i + 1 + ".");
+                    System.out.print("[" + tasks[i].getStatusIcon() + "] ");
+                    System.out.println(tasks[i].getDescription());
                 }
                 System.out.println("____________________________________________________________");
 
                 continue;
             }
 
+            String[] parts = input.split(" ");
+            if ((parts[0].equals("mark") || parts[0].equals("unmark")) && parts.length == 2) {
+                try {
+                    int index = Integer.parseInt(parts[1]);
+                    if (parts[0].equals("mark")) {
+                        tasks[index - 1].markAsDone();
+                        System.out.println("____________________________________________________________");
+                        System.out.println(" Nice! I've marked this task as done:");
+                        System.out.println("[X] " + tasks[index - 1].getDescription());
+                        System.out.println("____________________________________________________________");
+                        continue;
+                    }
+
+                    else {
+                        tasks[index - 1].markAsNotDone();
+                        System.out.println("____________________________________________________________");
+                        System.out.println(" OK, I've marked this task as not done yet:");
+                        System.out.println("[ ] " + tasks[index - 1].getDescription());
+                        System.out.println("____________________________________________________________");
+                        continue;
+                    }
+                } catch (Exception e) {
+
+                }
+
+            }
+
+
             System.out.println("____________________________________________________________");
             System.out.println("added: " + input);
             System.out.println("____________________________________________________________");
-            tasks[taskCount] = input;
+            tasks[taskCount] = new Task(input);
             taskCount++;
         }
+
         System.out.println("____________________________________________________________");
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
