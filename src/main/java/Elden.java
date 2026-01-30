10e689d266e42cae05e2e462d770fd0a788d0aa4
fromtyn <tianyini2023@gmail.com>
2026-01-30 22:46:50 +1100

    Complete Level-1 Echo

--- src/main/java/Elden.java
+++ src/main/java/Elden.java
@@ -1,8 +1,23 @@
+import java.util.Scanner;
+
 public class Elden {
     public static void main(String[] args) {
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Elden");
         System.out.println("What can I do for you?");
+
+        Scanner in = new Scanner(System.in);
+        while(true) {
+            String input = in.nextLine().trim();
+
+            if (input.equals("bye")) {
+                break;
+            }
+
+            System.out.println("____________________________________________________________");
+            System.out.println(input);
+            System.out.println("____________________________________________________________");
+        }
         System.out.println("____________________________________________________________");
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
