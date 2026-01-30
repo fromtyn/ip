64426af7b5b8aeac08678c333eb4bdf95b09de24
fromtyn <tianyini2023@gmail.com>
2026-01-30 23:09:43 +1100

    Complete Level-2 Add,List

--- src/main/java/Elden.java
+++ src/main/java/Elden.java
@@ -5,18 +5,34 @@ public class Elden {
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Elden");
         System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
 
         Scanner in = new Scanner(System.in);
+        String[] tasks = new String[100];
+        int taskCount = 0;
+
         while(true) {
-            String input = in.nextLine().trim();
+            String input = in.nextLine();
 
             if (input.equals("bye")) {
                 break;
             }
 
+            if (input.equals("list")) {
+                System.out.println("____________________________________________________________");
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println(i + 1 + ". " + tasks[i]);
+                }
+                System.out.println("____________________________________________________________");
+
+                continue;
+            }
+
             System.out.println("____________________________________________________________");
-            System.out.println(input);
+            System.out.println("added: " + input);
             System.out.println("____________________________________________________________");
+            tasks[taskCount] = input;
+            taskCount++;
         }
         System.out.println("____________________________________________________________");
         System.out.println("Bye. Hope to see you again soon!");
